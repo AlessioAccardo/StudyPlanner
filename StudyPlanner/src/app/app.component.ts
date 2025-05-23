5f2b0e5dacf5f87ace2d4d6fd4118650102d8a15
Gabrooh1 <gabrielesansonini1604@gmail.com>
2025-05-23 21:00:07 +0200

    Beta1

--- StudyPlanner/src/app/app.component.ts
+++ StudyPlanner/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FaqComponent } from './faq/faq.component';
+import { LoginComponent } from './login/login.component';
+import { PlanComponent } from './plan/plan.component';
+import { RequestComponent } from './request/request.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, DashboardComponent, FaqComponent, LoginComponent, PlanComponent, RequestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
