3ff1f583e7fddfcb325096b427e3f5b67c14140a
AlessioAccardo <alessiohrdo@gmail.com>
2025-06-20 18:26:25 +0200

    period

--- StudyPlanner/src/app/home/home.component.ts
+++ StudyPlanner/src/app/home/home.component.ts
@@ -1,12 +1,41 @@
-import { Component,inject } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-auth = inject(AuthService);
+  searchText = input<string>();
+  research: string = "";
+
+  esami: string[] = [
+    'INF001 - Analisi Matematica II',
+    'INF002 - Programmazione Web&Mobile',
+    'INF003 - Basi di Dati',
+    'INF004 - Ingegneria del Software',
+    'INF005 - Reti di Calcolatori'
+  ];
+
+  filteredEsami: string[] = [...this.esami];
+
+  onSearch(): void {
+    const research = this.searchText.toString()
+    const text = this.research.toLowerCase().trim();
+    if (!text) {
+      // se la stringa di ricerca è vuota, ripristino tutti
+      this.filteredEsami = [...this.esami];
+    } else {
+      this.filteredEsami = this.esami.filter(esame =>
+        esame.toLowerCase().includes(text)
+      );
+    }
+  }
 }
