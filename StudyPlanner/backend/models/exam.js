2438dcb5057faf47c8b4667cf84b3ce1d1d5565b
Gabrooh1 <gabrielesansonini1604@gmail.com>
2025-06-24 17:04:27 +0200

    funzioni risultati esame

--- StudyPlanner/backend/models/exam.js
+++ StudyPlanner/backend/models/exam.js
@@ -1,4 +1,3 @@
-const { rejects } = require('assert');
 const db = require('../db/database');
 
 class Exam {
@@ -60,4 +59,42 @@ class Exam {
             });
         });
     }
+
+
+    //funzione di inserimento dei risultati per ogni esame
+    async createExamResults(exam_code, student_id, grade){ 
+        return new Promise((resolve, reject) => {
+            db.run('INSERT INTO examsResults(exam_code, student_id, grade) VALUES (?,?,?)', [exam_code, student_id, grade], (err) => {
+                if(err) reject(err);
+                resolve({exam_code, student_id, grade});
+            });
+        });
+        }
+    
+
+    //funzione per i risultati di un singolo studente se il risultato è stato accettato (esame completato)
+    async getAcceptedResultsByStudent(student_id) {
+        return new Promise((resolve, reject) => {
+             db.all( 
+                'SELECT exam_code, grade FROM examResults WHERE student_id = ? AND accepted = 0', [student_id], (err, rows) => {
+                    if (err) return reject(err);
+                     resolve(rows)
+        });
+    });
+}
+
+    //funzione per tutti i risultati di un esame
+    async getExamResultsFromExam(exam_code) {
+        return new Promise((resolve, reject) => {
+            db.all(
+                'SELECT student_id, grade, accepted FROM examResults WHERE exam_code == ?',
+                [exam_code],
+                (err, rows) => {
+                    if(err) return reject(err);
+                    resolve(rows);
+                }
+            );
+        });
+    }
+
 }
