ab412406a1e99e72927c081b94dbbaff6923e3ce
AlessioAccardo <alessiohrdo@gmail.com>
2025-06-30 13:12:06 +0200

    login

--- StudyPlanner/src/app/interceptors/token.interceptor.ts
+++ StudyPlanner/src/app/interceptors/token.interceptor.ts
@@ -1,11 +1,12 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+/*import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
   const newReq = req.clone({
     setHeaders: {
-      Authorization: `Bearer ${token}`
+      Authorization: `token ${token}`
     }
   })
   return next(newReq);
 };
+*/
