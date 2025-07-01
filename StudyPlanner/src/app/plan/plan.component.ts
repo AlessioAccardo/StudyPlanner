cc769cbea463d726adea352bf3a36527a96ec0d0
albertoruoppolo <alberto.ruoppolo06@gmail.com>
2025-07-01 15:05:17 +0200

    ---

--- StudyPlanner/src/app/plan/plan.component.ts
+++ StudyPlanner/src/app/plan/plan.component.ts
@@ -51,11 +51,11 @@ export class PlanComponent implements OnInit{
   }
 
   get esamiFiltrati() {
-    if (!this.searchText.trim()) return this.esami;
-    return this.esami.filter(e =>
-      ${e.code} ${e.name}.toLowerCase().includes(this.searchText.toLowerCase())
-    );
-  }
+  if (!this.searchText.trim()) return this.esami;
+  return this.esami.filter(e =>
+    `${e.code} ${e.name}`.toLowerCase().includes(this.searchText.toLowerCase())
+  );
+}
 
   onSearch(event: Event): void {
     const input = event.target as HTMLInputElement;
