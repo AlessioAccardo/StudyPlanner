79fccc705196ae527e19d22d5c6f1639feb9c746
AlessioAccardo <alessiohrdo@gmail.com>
2025-07-01 14:06:46 +0200

    fix localstorage error

--- StudyPlanner/src/app/app.component.ts
+++ StudyPlanner/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet, Router, RouterLink} from '@angular/router';
 import { LoggedUser } from './interfaces/loggedUser.interface';
 import { CommonModule } from '@angular/common';
--- StudyPlanner/src/app/guard/auth.guard.ts
+++ StudyPlanner/src/app/guard/auth.guard.ts
@@ -1,14 +1,23 @@
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 export const authGuard: CanActivateFn = (route, state) => {
-   const isLoggedIn = !!localStorage.getItem('token');
-   const userLoggedIn = !!localStorage.getItem('currentUser');
-   const router = inject(Router);
-   if (isLoggedIn && userLoggedIn) {
+  const platformId = inject(PLATFORM_ID);
+  const router = inject(Router);
+
+  const isBrowser = isPlatformBrowser(platformId);
+  if (!isBrowser) {
+    return false;
+  }
+
+  const isLoggedIn = !!localStorage.getItem('token');
+  const userLoggedIn = !!localStorage.getItem('currentUser');
+
+  if (isLoggedIn && userLoggedIn) {
     return true;
-   } else {
+  } else {
     router.navigateByUrl('no-role');
     return false;
-   }
+  }
 };
